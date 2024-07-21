23d8558e1e47089e9cbcc941d9588a9ece2e6d81
TAFH-debug <aushahman2007@gmail.com>
2024-07-22 03:54:19 +0500

    v0.0.1

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "../globals.css";
 import Header from "../../components/header";
-import "dotenv/config";
 
 const inter = JetBrains_Mono({ subsets: ["latin"] });
 
