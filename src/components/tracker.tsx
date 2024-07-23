2cdc0f8b8f98f0550338183048d282d33f0b5d6c
TAFH-debug <aushahman2007@gmail.com>
2024-07-23 22:58:17 +0500

    yo

--- src/components/tracker.tsx
+++ src/components/tracker.tsx
@@ -4,11 +4,11 @@ export default function Tracker() {
     return ( <>
     <Script id="analytics1" async src="https://www.googletagmanager.com/gtag/js?id=G-DT7RTZTFLW"></Script>
     <Script id="analytics2">
-      window.dataLayer = window.dataLayer || [];
+      {`window.dataLayer = window.dataLayer || [];
       function gtag(){ (window as any).dataLayer.push(arguments) }
       gtag('js', new Date());
 
-      gtag('config', 'G-DT7RTZTFLW');
+      gtag('config', 'G-DT7RTZTFLW');`}
     </Script>
     </>)
 }
