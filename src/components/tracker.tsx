f857c770ce78bea7cb3418ae19b4cb1478e15e91
TAFH-debug <aushahman2007@gmail.com>
2024-07-23 22:57:09 +0500

    yo

--- src/components/tracker.tsx
+++ src/components/tracker.tsx
@@ -2,8 +2,8 @@ import Script from "next/script";
 
 export default function Tracker() {
     return ( <>
-    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-DT7RTZTFLW"></Script>
-    <Script>
+    <Script id="analytics1" async src="https://www.googletagmanager.com/gtag/js?id=G-DT7RTZTFLW"></Script>
+    <Script id="analytics2">
       window.dataLayer = window.dataLayer || [];
       function gtag(){ (window as any).dataLayer.push(arguments) }
       gtag('js', new Date());
